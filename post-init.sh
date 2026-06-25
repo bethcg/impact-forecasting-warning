7282f3467d29b24462b11ede8a633127de4db55f
bethcg <127967525+bethcg@users.noreply.github.com>
2026-06-25 23:29:01 +0200

    Remove redundant PROJ_LIB environment variable setup

--- post-init.sh
+++ post-init.sh
@@ -3,10 +3,3 @@
 # 1. Install additional dependencies via Poetry
 echo "Running poetry install..."
 poetry install
-
-# 2. Persist the PROJ_LIB environment variable for the user session
-echo "Setting PROJ_LIB environment variable..."
-export PROJ_LIB=/layers/paketo-buildpacks_conda-env-update/conda-env/share/proj
-
-# Append it to .bashrc so it persists in any new terminal tabs the user opens
-echo 'export PROJ_LIB=/layers/paketo-buildpacks_conda-env-update/conda-env/share/proj' >> ~/.bashrc
