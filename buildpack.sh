037d03e067e6a8e381a2bc9cd1a7eb6d0bf7e3d7
bethcg <127967525+bethcg@users.noreply.github.com>
2026-06-26 07:26:57 +0200

    Update buildpack.sh to include post-init script execution

--- buildpack.sh
+++ buildpack.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+# 1. Use the official Paketo launcher to execute your post-init environment setup script
 /cnb/lifecycle/launcher $SCRIPT_DIR/post-init.sh
+
 echo "" >> /home/renku/.bashrc
+
+# 2. Hand control back over to JupyterLab (or your default Renku interface) so the session stays open
+/cnb/process/vscodium
